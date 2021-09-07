dc4e1754ced94a543523d22f193df980befa7221
Manish Kumar Vats <mvats77947794@gmail.com>
2021-09-08 00:37:23 +0530

    Resolved the error by removinf kotlin-android-extension plugin and by adding new the viewBinding way

--- app/src/main/java/com/example/newsit/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/newsit/adapters/NewsAdapter.kt
@@ -3,36 +3,34 @@ package com.example.newsit.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.newsit.R
 import com.example.newsit.models.Article
-import kotlinx.android.synthetic.main.item_article_preview.view.*
 
-class NewsAdapter :RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>(){
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
-    inner class ArticleViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-    private val differCallBack=object :DiffUtil.ItemCallback<Article>(){
+    private val differCallBack = object : DiffUtil.ItemCallback<Article>() {
         override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
-            return oldItem.url==newItem.url
+            return oldItem.url == newItem.url
         }
 
         override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
-            return oldItem==newItem
+            return oldItem == newItem
         }
     }
 
-    val differ=AsyncListDiffer(this,differCallBack)
+    val differ = AsyncListDiffer(this, differCallBack)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
         return ArticleViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.item_article_preview,
-                parent,
-                false
-            )
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_article_preview, parent, false)
         )
     }
 
@@ -41,11 +39,12 @@ class NewsAdapter :RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>(){
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
         val article = differ.currentList[position]
         holder.itemView.apply {
-            Glide.with(this).load(article.urlToImage).into(ivArticleImage)
-            tvSource.text = article.source.name
-            tvTitle.text = article.title
-            tvDescription.text = article.description
-            tvPublishedAt.text = article.publishedAt
+            Glide.with(this).load(article.urlToImage)
+                .into(findViewById<ImageView>(R.id.ivArticleImage))
+            findViewById<TextView>(R.id.tvSource).text = article.source.name
+            findViewById<TextView>(R.id.tvTitle).text = article.title
+            findViewById<TextView>(R.id.tvDescription).text = article.description
+            findViewById<TextView>(R.id.tvPublishedAt).text = article.publishedAt
 
             setOnClickListener {
                 onItemClickListener?.let { it(article) }
--- app/src/main/java/com/example/newsit/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newsit/ui/fragments/BreakingNewsFragment.kt
@@ -2,63 +2,76 @@ package com.example.newsit.ui.fragments
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.newsit.R
 import com.example.newsit.adapters.NewsAdapter
+import com.example.newsit.databinding.FragmentBreakingNewsBinding
 import com.example.newsit.ui.NewsActivity
 import com.example.newsit.ui.NewsViewModel
 import com.example.newsit.util.Resource
-import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
-class BreakingNewsFragment:Fragment(R.layout.fragment_breaking_news) {
+class BreakingNewsFragment : Fragment() {
 
     lateinit var viewModel: NewsViewModel
     lateinit var newsAdapter: NewsAdapter
 
-    val TAG="BreakingNewsFragment"
+    //binding variable
+    private lateinit var binding: FragmentBreakingNewsBinding
+
+    val TAG = "BreakingNewsFragment"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBreakingNewsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         super.onViewCreated(view, savedInstanceState)
-        viewModel=(activity as NewsActivity).viewModel
+        viewModel = (activity as NewsActivity).viewModel
         setupRecyclerView()
 
-        viewModel.breakingNews.observe(viewLifecycleOwner, Observer {response ->
-        when(response){
-            is Resource.Success ->{
-                hideProgressBar()
-                response.data?.let { newsResponse ->
-                    newsAdapter.differ.submitList(newsResponse.articles)
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occured: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
                 }
             }
-            is Resource.Error ->{
-                hideProgressBar()
-                response.message?.let{message ->
-                    Log.e(TAG, "An error occured: $message")
-                }
-            }
-            is Resource.Loading -> {
-                showProgressBar()
-            }
-        }
         })
     }
 
-    private fun hideProgressBar(){
-        paginationProgressBar.visibility=View.INVISIBLE
+    private fun hideProgressBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
     }
 
-    private fun showProgressBar(){
-        paginationProgressBar.visibility=View.VISIBLE
+    private fun showProgressBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
     }
-    private fun setupRecyclerView(){
-        newsAdapter= NewsAdapter()
-        rvBreakingNews.apply {
-            adapter=newsAdapter
-            layoutManager=LinearLayoutManager(activity)
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        binding.rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
         }
     }
 }
